c3df6479a94d4dc89c1ddb6c849dc69bdde6c948
jbenetts <jbenetts@insulet.com>
2022-03-16 09:34:52 -0700

    Added Outlets for the items and a function for the slider

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Items
+    let storyPrompt = StoryPromptEntry()
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyPrompt = StoryPromptEntry()
         storyPrompt.noun = "toaster"
         storyPrompt.adjective = "smelly"
         storyPrompt.verb = "burps"
