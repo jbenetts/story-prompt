6cbe061889b534f7946ec47ca775193fe0c0e27c
jbenetts <jbenetts@insulet.com>
2022-03-16 11:30:45 -0700

    Added the asignment of the story type and comments

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -15,21 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var verbTextField: UITextField!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var numberSlider: UISlider!
+    //Action Items as functions
+    //Change the number label depending on the slider
     @IBAction func changeNumber(_ sender: UISlider) {
         numberLabel.text = "Number: \(Int(sender.value))"
         storyPrompt.number = Int(sender.value)
     }
+    //Assign a value depending on what is selected on the pill
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex){
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        numberSlider.value = 7.5
         storyPrompt.noun = "toaster"
         storyPrompt.adjective = "smelly"
         storyPrompt.verb = "burps"
-        storyPrompt.number = 10
+        storyPrompt.number = Int(numberSlider.value)
         print(storyPrompt)
         print("Hello World!")
         // Do any additional setup after loading the view.
     }
-
-
 }
 
